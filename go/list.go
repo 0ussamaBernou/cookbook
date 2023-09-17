02897125b4097007e4647848f2dee70aa56a30a3
0ussamabernou <oussama_bernou@um5.ac.ma>
2023-09-17 01:10:02 +0100

    update

---
+++ go/list.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+)
+
+// List represents a singly-linked list that holds
+// values of any type.
+type List[T any] struct {
+	next *List[T]
+	val  T
+}
+
+func (list *List[T]) display() string {
+	out := ""
+	iter := list
+	for iter != nil {
+		out += fmt.Sprintf("%v -> ", iter.val)
+		iter = iter.next
+	}
+	out += fmt.Sprintf("nil")
+	return out
+}
+
+func main() {
+	list := List[string]{next: nil, val: "first"}
+	list.next = &List[string]{next: nil, val: "second"}
+	list.next.next = &List[string]{next: nil, val: "third"}
+	fmt.Printf("%v", list.display())
+}
